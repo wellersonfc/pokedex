453f2ef6a5c0e982c4af6e2477c4687d1445405f
wellersonfc <wellersonfc@gmail.com>
2022-07-21 22:19:18 -0300

    base pokedex

---
+++ src/components/Botton/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+class Botton extends React.Component{
+    render(){
+        return(
+            <button></button>
+        );
+    }
+}
+
+export default Botton;
---
+++ src/components/pages/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Card from '../Card';
+import "./style.scss";
+import logo from  "./Pokédex_3D_logo.png";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <img src={logo} className="title-img"></img>
+        <div className='slider'>
+          <Card/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
