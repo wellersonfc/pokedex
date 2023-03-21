75622ea17d5cec1a54ff6491b3792e8bad4799b5
Wellerson Ferreira de Carvalho <wellersonfc@gmail.com>
2023-03-21 20:21:06 -0300

    add | adicionando bibliotecas

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import Card from '../Card';
 import "./style.scss";
 import logo from  "./Pokédex_3D_logo.png";
+import Navbar from '../components/Navebar';
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <img src={logo} className="title-img"></img>
-        <div className='slider'>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-        </div>
+      </div>
+      <div>
+        <Navbar />
       </div>
     </div>
   );
