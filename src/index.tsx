b3e7e314b2d563c10368b3a8ad9453c61febc2f5
Wellerson Ferreira de Carvalho <wellersonfc@gmail.com>
2023-04-04 16:50:24 -0300

    routers

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Home from "./pages/Home/"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Home />
+  }
+]);
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
 
 reportWebVitals();
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import styles from "./Home.module.css";
+
+const Home = () => {
+    return(
+        <div className={styles.container}>
+
+        </div>
+    );
+};
+
+export default Home;
